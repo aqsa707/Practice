0a340282f580f68bdf9311a666d7938fc7b1464b
aqsa707 <146213719+aqsa707@users.noreply.github.com>
2023-11-28 09:45:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// EVENT LISTENERS
+
+// Event listeners
+document.getElementById("btn1").addEventListener("click", function1);
+document.getElementById("btn2").addEventListener("click", function2);
+document.getElementById("btn3").addEventListener("click", function3);
+document.getElementById("btn4").addEventListener("click", function4);
+document.getElementById("btn5").addEventListener("click", function5);
+
+// Event Functions
+function function1() { 
+    alert("Trust what someone says");
+    }
+
+function function2() {
+        alert("Go to sleep");
+    }
+
+function function3() {
+    alert("It's not complicated");
+}
+
+function function4() {
+    alert("It's too late");
+}
+
+function function5 () {
+    alert("Sick");
+}
